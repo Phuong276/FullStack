527b8b952a7380ef6b64a9ed36f705d0cb72aae8
vuducphuong2001@gmail.com <vuducphuong2001@gmail,com>
2022-10-31 17:44:18 +0700

    push

--- NODEJS/src/controllers/userController.js
+++ NODEJS/src/controllers/userController.js
@@ -20,6 +20,53 @@ let handleLogin = async (req, res) => {
     })
 }
 
+let handleGetAllUsers = async (req, res) => {
+    let id = req.query.id;
+    let users = await userService.getAllUsers(id);
+
+    if(!id) {
+        return res.status(200).json( {
+            errCode: 1,
+            errMessage: 'Missing required parameters',
+            users: []
+        })
+    }
+
+    return res.status(200).json( {
+        errCode: 0,
+        errMessage: 'Ok',
+        users
+    })
+}
+
+let handleCreateNewUser = async (req, res) => {
+    let message = await userService.createNewUser(req.body)
+    return res.status(200).json( {
+        errMessage: message
+    })
+}
+
+let handleDeletetUser = async (req, res) => {
+    if(!req.body.id) {
+        return res.status(200).json( {
+            errCode: 1,
+            errMessage: "Missing requid parameters"
+        })
+    }
+    let message = await userService.deleteUser(req.body.id)
+    return res.status(200).json( {
+        errMessage: message
+    })
+}
+
+let handleEditUser = async (req, res) => {
+
+}
+
 module.exports = {
-    handleLogin: handleLogin
+    handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers,
+    handleCreateNewUser: handleCreateNewUser,
+    handleEditUser: handleEditUser,
+    handleDeletetUser: handleDeletetUser,
 }
